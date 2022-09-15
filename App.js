e834574655bce646f431057495dd41d5d7452f20
DanielDante15 <danieldantefm@gmail.com>
2022-09-15 11:32:00 -0300

    adding things

--- App.js
+++ App.js
@@ -1,22 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import TelaLogin from './src/Telas/tela_inicial';
-
+import React, { useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { Button } from "react-native-web";
+import { TextInput } from "react-native";
 
 export default function App() {
+  const [cont, setCont] = useState(0);
+  const [resultado,setResultado]=useState('')
+  const [operacao,setOperacao]=useState('')
+  const operar = ()=> (setResultado(eval(operacao)))
+
+
+  const contar = () => {setCont(cont + 1)};
+
   return (
     <View style={styles.container}>
-      <StatusBar style="auto" />
-      <TelaLogin/>
+      <Button title="Teste" onPress={() => contar()} />
+      <Text>{cont}</Text>
+
+      <TextInput
+        style={styles.input}
+        value={String((operacao))}
+        placeholder="Faça seu calculo"
+        onChangeText={(m)=>{setOperacao(m)}}
+      />
+      <TextInput
+        placeholder="Resultado"
+        value={String((resultado))}   
+        style={styles.input}   
+      />
+
+      <Button
+      title = '='
+      onPress ={()=>operar()}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
   },
 });
